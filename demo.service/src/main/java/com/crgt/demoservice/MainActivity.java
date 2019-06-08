cde417c7fb7d708bfdc247723a426acc4ab97f76
jesse <jesse_lu@foxmail.com>
2019-06-08 14:31:02 +0800

    主模块通过maven依赖子模块,测试服务可用性

--- demo.service/src/main/java/com/crgt/demoservice/MainActivity.java
+++ demo.service/src/main/java/com/crgt/demoservice/MainActivity.java
@@ -28,14 +28,16 @@ public class MainActivity extends Activity {
         String userToken = userInfoService.getUserToken();
         Log.e(TAG, "userId: " + userId + " userToken: " + userToken);
 
+
+        //调用其他模块服务, 返回复杂类型
+        ObjectService objectService = ServiceManager.findService(ObjectService.class);
+        Log.d(TAG, "objectService:" + objectService.getObject().id);
+
         //调用本模块服务, 返回简单类型
         SimpleTypeService simpleTypeService = ServiceManager.findService(SimpleTypeService.class);
         int unPublishCount = simpleTypeService.getInt();
         Log.e(TAG, "simpleTypeService: " + unPublishCount);
 
-        //调用其他模块服务, 返回复杂类型
-        ObjectService objectService = ServiceManager.findService(ObjectService.class);
-        Log.d(TAG, "objectService:" + objectService.getObject().id);
 
 
         objectService.getObject().id = 102;
